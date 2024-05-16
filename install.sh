e2c3e01d4b36fe3c67636d6fa742ef128acf8e88
Joyal George <joyalgeorgepulk@gmail.com>
2024-05-16 23:21:44 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+# Check if Node.js is already installed
+if command -v node &>/dev/null; then
+    echo "Node.js is already installed."
+else
+    echo "Node.js is not installed"
+    exit 0
+fi
+
 # Display installed Node.js and npm versions
 echo "NODE VERSION = ";node -v
 echo "NPM VERSION = ";npm -v
+
+# Insalling Node Packages
+npm i
