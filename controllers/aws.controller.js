c4265ddbd43847d3ea107d74095bc2151561b968
Joyal George <joyalgeorgepulk@gmail.com>
2024-05-25 23:46:57 +0530

    aws s3 upload

--- controllers/aws.controller.js
+++ controllers/aws.controller.js
@@ -1,6 +1,26 @@
+const AWS = require('../services/aws.service')
+
 async function uploadFile(req, res, next){
     try {
-        res.send(req.file)
+        if(!req.file){
+            return res.status(400).json({status : false, message : 'Please select a image'})
+        }
+        const aws = new AWS()
+        let fileType = req.file.originalname.split(".")[1]
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET,
+            Key: `test.${fileType}`,
+            Body: req.file.buffer,
+            ContentType: req.file.mimetype
+        }
+        // console.log(params);  
+        let dpKey = await aws.uploadToS3(params)
+        res.send(dpKey)
+        // await User.findOneAndUpdate({ _id: req.userid }, {dp: dpKey}, {select: '-password'})
+        // let data = {
+        //     url: getProfilePicture(params.Key)
+        // }
+        // res.status(200).json({status : true, data: data, message : 'Profile picture uploaded successfully'})
     } catch (error) {
         next(error)
     }
--- services/aws.service.js
+++ services/aws.service.js
@@ -4,9 +4,22 @@ class AWS{
     constructor(){
         aws.config.update({
             accessKeyId: process.env.AWS_ACCESS_KEY,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            secretAccessKey: process.env.AWS_SECRET_KEY,
             region: process.env.AWS_REGION
         });
+    }
+
+    async uploadToS3(params) {
+        try {
+            console.log("uploading to s3 staered");
+            const s3 = new aws.S3()
+            const result = await s3.upload(params).promise()
+            console.log(result);
+            return result.Key
+        } catch (error) {
+            console.log(error);
+            throw error
+        }
     }
 }
 
